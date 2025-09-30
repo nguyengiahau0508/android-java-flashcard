cf330e5d0db95a8b86f4b86dbadba3a2b803f9b1
Nguyen Gia Hau <nguyengiahau2004@gmail.com>
2025-09-30 13:47:59 +0700

    feat: add settings icon (ic_settings.xml)

--- app/src/main/java/com/example/flashcard/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/flashcard/ui/home/HomeActivity.java
@@ -4,14 +4,31 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.flashcard.R;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewDecks;
+    private FloatingActionButton fabAddDeck;
+    private MaterialToolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerViewDecks = findViewById(R.id.recyclerViewDecks);
+        recyclerViewDecks.setLayoutManager(new LinearLayoutManager(this));
+
+        fabAddDeck = findViewById(R.id.fabAddDeck);
     }
 }
